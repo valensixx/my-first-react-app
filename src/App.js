324165f74a0b74dd9cc4d0cf3e5d27cb83c6801e
Valen Black <valensixx@gmail.com>
2023-02-13 23:37:02 +0200

    movie poster check

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ const App = () => {
                 </div>
 
                 <div>
-                    <img src={movie1.Poster} alt={movie1.Title}/>
+                    <img src={movie1.Poster !== 'N/A' ? movie1.Poster: 'https://via.placeholder.com/400'} alt={movie1.Title}/>
                 </div>
             </div>
         </div>
