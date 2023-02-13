2611843b94c28514864c476abbe2eb73f24b493d
Valen Black <valensixx@gmail.com>
2023-02-13 23:32:43 +0200

    add

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import SearchIcon from './search.svg';
 
 const API_URL = 'https://www.omdbapi.com?apikey=177d1d8e';
 
+const movie1 = {   //this object is taken to be static from the api at the console in the web brawser
+    "Title": "Batman v Superman: Dawn of Justice",
+    "Year": "2016",
+    "imdbID": "tt2975590",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+    }   
+
 const App = () => {
 
     const searchMovies = async (title) => {
@@ -32,6 +40,18 @@ const App = () => {
                     alt = "search icon img"
                     onClick={() => {} }
                 />
+            </div>
+
+            <div className="container"> 
+                <div className="movie">
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+                </div>
+
+                <div>
+                    <img src={movie1.Poster} alt={movie1.Title}/>
+                </div>
             </div>
         </div>
     );
