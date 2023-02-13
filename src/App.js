df6c6145d1c57f68d04e090ba13c70c02a47c608
Valen Black <valensixx@gmail.com>
2023-02-13 21:07:32 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import React from "react";
+import {useEffect} from "react";
+
+const API_URL = 'https://www.omdbapi.com?apikey=177d1d8e';
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(``);
+    }
+
+    useEffect(() =>{
+
+    }, []);
+
     return(
         <h1>App</h1>
     );
