9d93bc1d755c4ff2af0a3101da6beed9d249ca60
Valen Black <valensixx@gmail.com>
2023-02-13 21:15:17 +0200

    my next commit

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ const API_URL = 'https://www.omdbapi.com?apikey=177d1d8e';
 const App = () => {
 
     const searchMovies = async (title) => {
-        const response = await fetch(``);
+        const response = await fetch(`${API_URL}&s=${title}`); //this line will call the API response
+        const data = await response.json();
+
+        console.log(data.Search); //I want to see only the search results. In my case all Batman movies
     }
 
     useEffect(() =>{
-
+        searchMovies('Batman');
     }, []);
 
     return(
