55b553906c2dbd78eaa8ece53419349ee97f3ac3
Valen Black <valensixx@gmail.com>
2023-02-14 17:58:09 +0200

    import movie card component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {useEffect} from "react";
+import MovieCard from "./MovieCard";
 import './App.css';
 import SearchIcon from './search.svg';
 
@@ -43,21 +44,7 @@ const App = () => {
             </div>
 
             <div className="container"> 
-                <div className="movie">
-                    <div>
-                        <p>{movie1.Year}</p>
-                    </div>
-                
-                    <div>
-                        <img src={movie1.Poster !== 'N/A' ? movie1.Poster: 'https://via.placeholder.com/400'} alt={movie1.Title}/>
-                    </div>
-
-                    <div>
-                        <span>{movie1.Type}</span>
-                        <h3>{movie1.Title}</h3>
-                    </div>
-
-                </div>
+                <MovieCard />
             </div>
         </div>
     );
